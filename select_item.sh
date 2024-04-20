e8c85e05870d1f098716524abec9fefc56b1b10f
Gabriel Touzalin <gabriel.touzalin@gmail.com>
2024-04-20 11:29:59 +0200

    Fixed Issue #3, select_items.sh changed

--- select_item.sh
+++ select_item.sh
@@ -1,29 +1,29 @@
 select_item_f() {
-	if [ ! -d "../srcs" ]; then
-		echo -e "\e[31mThe directory ../srcs is missing.\e[0m" >&1
-		exit 1
-	fi
+    if [ ! -d "../srcs" ]; then
+        echo -e "\e[31mThe directory ../srcs is missing.\e[0m" >&1
+        exit 1
+    fi
 
-	items=()
-	while IFS= read -r -d '' item; do
-		if [ -d "$item" ]; then
-			items+=("$item")
-		fi
-	done < <(find "../srcs" -type d -print0)
-	wait
+    items=()
+    while IFS= read -r -d '' item; do
+        if [ -d "$item" ] && [[ ! "$item" == */.* ]]; then
+            items+=("$item")
+        fi
+    done < <(find "../srcs" -type d ! -path '*/\.*' -print0)
+    wait
 
-	echo -e "\e[33mSelect a directory:\e[0m" >&1
-	select item in "${items[@]}"; do
-		if [ -n "$item" ]; then
-			echo -e "\e[32m$item : selected\e[0m" >&1
-			wait
-			break
-		else
-			echo -e "\e[31mInvalid selection. Please try again.\e[0m" >&1
-		fi
-	done
-	local -n var=$1
-	var="$item"
+    echo -e "\e[33mSelect a directory:\e[0m" >&1
+    select item in "${items[@]}"; do
+        if [ -n "$item" ]; then
+            echo -e "\e[32m$item : selected\e[0m" >&1
+            wait
+            break
+        else
+            echo -e "\e[31mInvalid selection. Please try again.\e[0m" >&1
+        fi
+    done
+    local -n var=$1
+    var="$item"
 }
 
 print_items() {
